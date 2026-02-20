719f1e9eab9f4b0e4c88672ffa004b4957c93a6a
Steffan Norberhuis <steffan@norberhuis.nl>
2026-02-20 10:30:23 +0100

    refactor: remove bootstrap:compliant from base template
    Compliant bootstrapping is centralized in platform-cdk, which now
    uses a generated CloudFormation template approach.

--- src/common/package-json.ts
+++ src/common/package-json.ts
@@ -24,8 +24,6 @@ export const CDK_SCRIPTS = {
   'synth': 'cdk synth --output cdk.out/$0/ --app "yarn ts-node --prefer-ts-exts bin/$0.ts";',
   'list': 'cdk list --output cdk.out/$0/ --app "yarn ts-node --prefer-ts-exts bin/$0.ts";',
   'bootstrap': 'cdk bootstrap --output cdk.out/$0/ --app "yarn ts-node --prefer-ts-exts bin/$0.ts";',
-  'bootstrap:compliant':
-    'cdk bootstrap --output cdk.out/$0/ --bootstrap-kms-key-id $(aws cloudformation list-exports --query "Exports[?Name==\'Platform-CompanyKeyId-v1\'].Value" --output text) --app "yarn ts-node --prefer-ts-exts bin/$0.ts";',
   'diff': 'cdk diff --output cdk.out/$0/ -e --app "yarn ts-node --prefer-ts-exts bin/$0.ts" ${1:---all};',
   'diff:ci': 'cdk diff --ci --app "yarn ts-node --prefer-ts-exts $0";',
   'deploy':
